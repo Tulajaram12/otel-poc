164159162a6aa0a8ac250f1aa2ef580b109183cd
Tulajaram12 <96332761+Tulajaram12@users.noreply.github.com>
2026-07-08 11:28:55 +0530

    Refactor tracing.js for OpenTelemetry configuration

--- app/tracing.js
+++ app/tracing.js
@@ -1,28 +1,21 @@
 'use strict';
 
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const {
-  getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
-const {
-  OTLPTraceExporter,
-} = require('@opentelemetry/exporter-trace-otlp-grpc');
-const {
-  OTLPMetricExporter,
-} = require('@opentelemetry/exporter-metrics-otlp-grpc');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 
 const sdk = new NodeSDK({
-  serviceName: 'nodejs-otel-demo',
+    serviceName: 'otel-node-demo',
 
-  traceExporter: new OTLPTraceExporter({
-    url: 'http://otel-collector:4317',
-  }),
+    traceExporter: new OTLPTraceExporter({
+        url: 'http://otel-collector:4317'
+    }),
 
-  metricReader: undefined,
-
-  instrumentations: [getNodeAutoInstrumentations()],
+    instrumentations: [
+        getNodeAutoInstrumentations()
+    ]
 });
 
 sdk.start();
 
-console.log('OpenTelemetry Initialized');
+console.log("OpenTelemetry initialized");
